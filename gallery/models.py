608f5f5fa49aa04e7b3a6f4f1da75be2abec8253
hillarydalie <hidalie@gmail.com>
2019-10-07 23:21:30 +0300

    Add image model to models.py

--- gallery/models.py
+++ gallery/models.py
@@ -16,7 +16,7 @@ class User(models.Model):
 
     def delete_user(self):
         self.delete()
-        
+
 class Category(models.Model):
     tag = models.CharField(max_length =30)
 
@@ -49,4 +49,39 @@ class Location(models.Model):
         self.save()
 
     def delete_location(self):
+        self.delete()
+
+class Image(models.Model):
+    title = models.CharField(max_length =60)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'images/', default='default.png')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,  blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
+
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def todays_images(cls):
+        today = dt.date.today()
+        photos = cls.objects.filter(pub_date__date = today)
+        return photos
+
+    @classmethod
+    def get_photos(cls):
+        photos = cls.objects.all()
+        return photos
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        photos = cls.objects.filter(title__icontains=search_term)
+        return photos
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
         self.delete()
