6374cb87c67c1e5a961ea1008152d0ab02a80e68
hillarydalie <hidalie@gmail.com>
2019-10-07 23:20:59 +0300

    Edit changes in models.py

--- gallery/models.py
+++ gallery/models.py
@@ -16,3 +16,37 @@ class User(models.Model):
 
     def delete_user(self):
         self.delete()
+        
+class Category(models.Model):
+    tag = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.tag
+    
+    @classmethod
+    def search_by_image_category(cls, search_term):
+        photos = cls.objects.filter(name__icontains=search_term)
+        return photos
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+class Location(models.Model):
+    place = models.CharField(max_length =30)
+
+    @classmethod
+    def search_by_image_location(cls, search_term):
+        photos = cls.objects.filter(location__icontains=search_term)
+        return photos
+
+    def __str__(self):
+        return self.place
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
