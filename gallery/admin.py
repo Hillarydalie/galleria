b77dced5b1ba94235bb7fd002afc9e24ceb39602
hillarydalie <hidalie@gmail.com>
2019-10-07 23:20:03 +0300

    Register model service in project.

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User,Image,Category,Location
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
