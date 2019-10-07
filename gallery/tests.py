154dcdaad99ddc3f80f57bc59daf0d4afe4314fc
hillarydalie <hidalie@gmail.com>
2019-10-07 23:16:39 +0300

    Add test for user class in the project.

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import datetime as dt
+from .models import User, Location, Category, Image
 
-# Create your tests here.
+
+# Test case for User class
+class UserTestClass(TestCase):
+    def setUp(self):
+        self.user = User(first_name = "John", last_name="Muriuki", email = "hidalie@gmail.com", phone_number="0722334455")
+        self.user.save()
+
+    def test_user_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_delete_user_method(self):
+        self.user.save_user()
+        users = User.objects.all()
+        self.user.delete_user()
+        users = User.objects.all()
+        self.assertTrue(len(users) == 0)
